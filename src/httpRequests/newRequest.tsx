641e64fbe4030ced1506a20c2ac05524e7666017
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-01 07:39:18 +0545

    hi commit

--- src/httpRequests/newRequest.tsx
+++ src/httpRequests/newRequest.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Props {
+  quotes: Myprops[];
+}
+
+interface Myprops {
+  id: number;
+  quote: string;
+  author: string;
+  // Other properties
+}
 
 export default function NewHttpRequest() {
+  const [data, setData] = useState<Props| null>(null);
+
+  useEffect(() => {
+    axios
+      .get<Props>("https://dummyjson.com/quotes")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error('API error', error);
+      });
+  }, []);
+
   return (
-    <div>
-              
+    <div >
+      {data?.quotes.map((curr) => (
+        <div key={curr.id} style={{textAlign:'center'}} >
+          <h3>{curr.quote}</h3>
+          <h2>- {curr.author}</h2>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
+
+
+// while fetching api do it inside useeffect hook either fetch inside or do it inside function and than pass that function inside the useEffect hook..
+//  first declare the kind of data that you are going to get when fetching the data..
+
+
+
+// Api for fetching photos thumbnail , 
+// 
+// if middle name chha vaney display garney nava na garney using conditional rendering.
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
-import { ProductList } from './listingProducts/listing'
+// import { ProductList } from './listingProducts/listing'
+import NewHttpRequest from './httpRequests/newRequest'
 // import { Login } from './Ant_design/login'
 
 
@@ -12,7 +13,8 @@ import { ProductList } from './listingProducts/listing'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <Login/> */}
-    <ProductList/>
+    {/* <ProductList/> */}
+    <NewHttpRequest/>
   </React.StrictMode>,
 )
   
